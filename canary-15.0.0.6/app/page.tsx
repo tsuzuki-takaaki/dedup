39f08a87e4ace7c28eef23256ca75fe59d6be199
tsuzuki-takaaki <echo.tsuzuki.takaaki@gmail.com>
2024-07-30 02:18:23 +0900

    Use v15.0.0-canary.6

---
+++ canary-15.0.0.6/app/page.tsx
@@ -0,0 +1,13 @@
+import { ComponentA } from "./components/component-a";
+import { ComponentB } from "./components/component-b";
+import { ComponentC } from "./components/component-c";
+
+export default function Home() {
+  return (
+    <main>
+      <ComponentA />
+      <ComponentB />
+      <ComponentC />
+    </main>
+  );
+}
